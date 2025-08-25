242106223741b7dd59b873b4669f4b5409efeb9e
Oscar Moreno <oscarpol95@gmail.com>
2025-08-25 17:00:04 -0500

    feat: implement PostgreSQL database configuration and logging setup

--- core_apps/user_auth/views.py
+++ core_apps/user_auth/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.views import View
+from loguru import logger
 
-# Create your views here.
+
+class TestLoggingView(View):
+  def get(self, request):
+    logger.debug("This is a debug message")
+    logger.info("this is an info message")
+    logger.warning("This is a warning message")
+    logger.error("This is an error message")
+    logger.critical("This is a critical message")
+    return JsonResponse({"message": "We are testing logging"})
